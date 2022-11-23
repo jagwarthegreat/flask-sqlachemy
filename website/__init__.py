18cc5d83a18bbeaf75b64b6946961fa4abeb9ecd
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-11-23 22:51:23 +0800

    initial commit template

---
+++ website/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
+    # NOTE: if password is present : 'mysql://user:password@host/database'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/flask'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    # this will create the models schema
+    # create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
